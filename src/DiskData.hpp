a10fd8a072230595b898a42643329daad30353b1
Unknown <daniellodelapedro@interia.pl>
2018-01-19 01:00:33 +0100

    Serializable models

--- src/DiskData.hpp
+++ src/DiskData.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "DeviceData.hpp"
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
 #include <string>
 
 using namespace std;
@@ -17,5 +19,20 @@ struct DiskData : DeviceData {
 	float disk_read_kbytes_per_sec;
 	float disk_write_kbytes_per_sec;
 	int disk_percentage_usage;
+
+	template<class Archive>
+	void serialize(Archive & ar, const unsigned int version)
+	{
+		ar & disk_model;
+		ar & disk_partitions_number;
+		ar & disk_size;
+		ar & disk_free_space;
+		ar & disk_current_queue_length;
+		ar & disk_avg_queue_length;
+		ar & disk_read_kbytes_per_sec;
+		ar & disk_write_kbytes_per_sec;
+		ar & disk_percentage_usage;
+	}
+
 };
 

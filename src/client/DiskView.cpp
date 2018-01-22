6d8b5ca2bf4827297bb4732a737380443282ef05
Unknown <lidka.wojciechowska1996@gmail.com>
2018-01-22 18:30:44 +0100

    DiskView and RAMView added

---
+++ src/client/DiskView.cpp
@@ -0,0 +1,16 @@
+#include "DiskView.hpp"
+
+DiskView::~DiskView()
+{
+}
+
+void DiskView::update()
+{
+	pwin->call_function("View.setDiskDetails1", sciter::value(disk->disk_percentage_usage),
+		sciter::value(disk->disk_size),
+		sciter::value(disk->disk_free_space),
+		sciter::value(disk->disk_partitions_number));
+	pwin->call_function("View.setDiskDetails2", sciter::value(disk->disk_write_kbytes_per_sec),
+		sciter::value(disk->disk_read_kbytes_per_sec),
+		sciter::value(disk->disk_current_queue_length));
+}
